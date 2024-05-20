cdd23a30c5bf233ddac9ffad0ccccf1ad3b82488
Adrixdd1 <rubioalvaradoadrian@gmail.com>
2024-05-20 16:17:43 -0600

    Added: New Proyect module

---
+++ src/app/nuevo-proyecto/nuevo-proyecto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nuevo-proyecto',
+  standalone: true,
+  imports: [],
+  templateUrl: './nuevo-proyecto.component.html',
+  styleUrl: './nuevo-proyecto.component.css'
+})
+export class NuevoProyectoComponent {
+
+}
